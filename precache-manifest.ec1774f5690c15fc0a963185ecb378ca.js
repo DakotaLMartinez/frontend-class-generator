d342e3ba689998542076c67e8d081d8706f4c41a
DakotaLMartinez <dakotaleemusic@gmail.com>
2019-09-27 19:02:24 -0700

    Updates

---
+++ precache-manifest.ec1774f5690c15fc0a963185ecb378ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3dc732072bab7978af6b3a37abf205",
+    "url": "/frontend-class-generator/index.html"
+  },
+  {
+    "revision": "b5ad9bff9f6de81fa5d1",
+    "url": "/frontend-class-generator/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4593b4037c5aad89194c",
+    "url": "/frontend-class-generator/static/js/2.9166519c.chunk.js"
+  },
+  {
+    "revision": "b5ad9bff9f6de81fa5d1",
+    "url": "/frontend-class-generator/static/js/main.c14f6913.chunk.js"
+  },
+  {
+    "revision": "d5edb2d67708222a5759",
+    "url": "/frontend-class-generator/static/js/runtime-main.dceb7b9a.js"
+  }
+]);
